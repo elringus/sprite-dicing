ff437223ced635fd73f4d637f1c66c932107bf92
Artyom Sovetnikov <elringus@yandex.ru>
2017-09-22 23:48:36 +0300

    update exporter

--- Assets/Editor/PackageExporter.cs
+++ Assets/Editor/PackageExporter.cs
@@ -16,18 +16,25 @@ public class PackageExporter : EditorWindow
     private static string outputFileName = PACKAGE_NAME;
     private static string assetsPath = "Assets/" + PACKAGE_NAME;
     private static string namespaceToWrap = PACKAGE_NAME;
-    private static string copyright = "// Copyright 2016-2017 Elringus (Artyom Sovetnikov). All Rights Reserved.";
+    private static string copyright = "// Copyright 2012-2017 Elringus (Artyom Sovetnikov). All Rights Reserved.";
     private static string tabChars = "    ";
     private static Dictionary<string, string> modifiedScripts = new Dictionary<string, string>();
 
-    [MenuItem("Assets/Export Custom Package", priority = 20)]
-    public static void ExportNovelEngine ()
+    [MenuItem("Assets/+ Export Package", priority = 20)]
+    public static void ExportPackage ()
     {
         if (IsReadyToExport)
             Export();
     }
 
-    private static void Export ()
+    [MenuItem("Assets/+ Export Package (Store)", priority = 20)]
+    public static void ExportPackageStore ()
+    {
+        if (IsReadyToExport)
+            Export(true);
+    }
+
+    private static void Export (bool wrapNamespace = false)
     {
         if (!string.IsNullOrEmpty(namespaceToWrap))
         {
@@ -41,9 +48,10 @@ public class PackageExporter : EditorWindow
 
                 string scriptText = string.Empty;
                 var isImportedScript = path.Contains("ThirdParty");
-                if (!isImportedScript) scriptText += copyright + Environment.NewLine + Environment.NewLine + "namespace " + namespaceToWrap + Environment.NewLine + "{" + Environment.NewLine;
+                var copyright = isImportedScript ? string.Empty : PackageExporter.copyright;
+                if (!isImportedScript || wrapNamespace) scriptText += copyright + Environment.NewLine + Environment.NewLine + "namespace " + namespaceToWrap + Environment.NewLine + "{" + Environment.NewLine;
                 scriptText += isImportedScript ? originalScriptText : tabChars + originalScriptText.Replace(Environment.NewLine, Environment.NewLine + tabChars);
-                if (!isImportedScript) scriptText += Environment.NewLine + "}" + Environment.NewLine;
+                if (!isImportedScript || wrapNamespace) scriptText += Environment.NewLine + "}" + Environment.NewLine;
                 File.WriteAllText(fullpath, scriptText, Encoding.UTF8);
 
                 modifiedScripts.Add(fullpath, originalScriptText);
