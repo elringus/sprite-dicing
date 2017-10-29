4ec1f1b6e72609bfdf915eb07f69a2f0ef68c5d1
Artyom Sovetnikov <elringus@yandex.ru>
2017-10-29 23:00:54 +0300

    update exporter

--- Assets/Editor/PackageExporter.cs
+++ Assets/Editor/PackageExporter.cs
@@ -1,71 +1,104 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
-using UnityEditor;
-using UnityEngine;
+﻿// Copyright 2012-2017 Elringus (Artyom Sovetnikov). All Rights Reserved.
 
-[InitializeOnLoad]
-public class PackageExporter : EditorWindow
+namespace UnityCommon
 {
-    private static readonly string PACKAGE_NAME = Application.productName;
-
-    private static bool IsReadyToExport { get { return !string.IsNullOrEmpty(outputPath) && 
-                                                !string.IsNullOrEmpty(outputFileName); } }
-    private static string outputPath = "C:/Users/Elringus/Desktop";
-    private static string outputFileName = PACKAGE_NAME;
-    private static string assetsPath = "Assets/" + PACKAGE_NAME;
-    private static string namespaceToWrap = PACKAGE_NAME;
-    private static string copyright = "// Copyright 2017 Elringus (Artyom Sovetnikov). All Rights Reserved.";
-    private static string tabChars = "    ";
-    private static Dictionary<string, string> modifiedScripts = new Dictionary<string, string>();
-
-    [MenuItem("Assets/+ Export Package", priority = 20)]
-    public static void ExportPackage ()
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+    using UnityEditor;
+    using UnityEngine;
+    
+    [InitializeOnLoad]
+    public class PackageExporter : EditorWindow
     {
-        if (IsReadyToExport)
-            Export();
-    }
-
-    [MenuItem("Assets/+ Export Package (Store)", priority = 20)]
-    public static void ExportPackageStore ()
-    {
-        if (IsReadyToExport)
-            Export(true);
-    }
-
-    private static void Export (bool wrapNamespace = false)
-    {
-        if (!string.IsNullOrEmpty(namespaceToWrap))
+        protected static string PackageName { get { return PlayerPrefs.GetString(PREFS_PREFIX + "PackageName"); } set { PlayerPrefs.SetString(PREFS_PREFIX + "PackageName", value); } }
+        protected static string Copyright { get { return PlayerPrefs.GetString(PREFS_PREFIX + "Copyright"); } set { PlayerPrefs.SetString(PREFS_PREFIX + "Copyright", value); } }
+        protected static string AssetsPath { get { return "Assets/" + PackageName; } }
+        protected static string OutputPath { get { return PlayerPrefs.GetString(PREFS_PREFIX + "OutputPath"); } set { PlayerPrefs.SetString(PREFS_PREFIX + "OutputPath", value); } }
+        protected static string OutputFileName { get { return PackageName; } }
+        protected static string NamespaceToWrap { get { return PackageName; } }
+        protected static bool IsReadyToExport { get { return !string.IsNullOrEmpty(OutputPath) && !string.IsNullOrEmpty(OutputFileName); } }
+    
+        private const string PREFS_PREFIX = "PackageExporter.";
+        private const string TAB_CHARS = "    ";
+    
+        private static Dictionary<string, string> modifiedScripts = new Dictionary<string, string>();
+    
+        private void Awake ()
         {
-            foreach (var path in AssetDatabase.GetAllAssetPaths())
+            if (string.IsNullOrEmpty(PackageName))
+                PackageName = Application.productName;
+        }
+    
+        [MenuItem("Edit/Project Settings/Package Exporter")]
+        private static void OpenSettingsWindow ()
+        {
+            var window = GetWindow<PackageExporter>();
+            window.Show();
+        }
+    
+        [MenuItem("Assets/+ Export Package", priority = 20)]
+        private static void ExportPackage ()
+        {
+            if (IsReadyToExport)
+                Export();
+        }
+    
+        [MenuItem("Assets/+ Export Package (Wrap)", priority = 20)]
+        private static void ExportPackageStore ()
+        {
+            if (IsReadyToExport)
+                Export(true);
+        }
+    
+        private void OnGUI ()
+        {
+            EditorGUILayout.LabelField("Package Exporter Settings", EditorStyles.boldLabel);
+            EditorGUILayout.LabelField("Settings are stored in editor's PlayerPrefs and won't be exposed in builds or project assets.", EditorStyles.miniLabel);
+            EditorGUILayout.Space();
+            PackageName = EditorGUILayout.TextField("Package Name", PackageName);
+            Copyright = EditorGUILayout.TextField("Copyright Notice", Copyright);
+            using (new EditorGUILayout.HorizontalScope())
             {
-                if (!path.StartsWith(assetsPath)) continue;
-                if (!path.EndsWith(".cs")) continue;
-
-                var fullpath = Application.dataPath.Replace("Assets", "") + path;
-                var originalScriptText = File.ReadAllText(fullpath, Encoding.UTF8);
-
-                string scriptText = string.Empty;
-                var isImportedScript = path.Contains("ThirdParty");
-                var copyright = isImportedScript ? string.Empty : PackageExporter.copyright;
-                if (!isImportedScript || wrapNamespace) scriptText += copyright + Environment.NewLine + Environment.NewLine + "namespace " + namespaceToWrap + Environment.NewLine + "{" + Environment.NewLine;
-                scriptText += isImportedScript ? originalScriptText : tabChars + originalScriptText.Replace(Environment.NewLine, Environment.NewLine + tabChars);
-                if (!isImportedScript || wrapNamespace) scriptText += Environment.NewLine + "}" + Environment.NewLine;
-                File.WriteAllText(fullpath, scriptText, Encoding.UTF8);
-
-                modifiedScripts.Add(fullpath, originalScriptText);
+                OutputPath = EditorGUILayout.TextField("Output Path", OutputPath);
+                if (GUILayout.Button("Select", EditorStyles.miniButton, GUILayout.Width(65)))
+                    OutputPath = EditorUtility.OpenFolderPanel("Output Path", "", "");
             }
         }
-
-        AssetDatabase.ExportPackage(assetsPath, outputPath + "/" + outputFileName + ".unitypackage", ExportPackageOptions.Recurse);
-
-        if (!string.IsNullOrEmpty(namespaceToWrap))
+    
+        private static void Export (bool wrapNamespace = false)
         {
-            foreach (var modifiedScript in modifiedScripts)
+            if (!string.IsNullOrEmpty(NamespaceToWrap))
             {
-                File.WriteAllText(modifiedScript.Key, modifiedScript.Value, Encoding.UTF8);
+                foreach (var path in AssetDatabase.GetAllAssetPaths())
+                {
+                    if (!path.StartsWith(AssetsPath)) continue;
+                    if (!path.EndsWith(".cs")) continue;
+    
+                    var fullpath = Application.dataPath.Replace("Assets", "") + path;
+                    var originalScriptText = File.ReadAllText(fullpath, Encoding.UTF8);
+    
+                    string scriptText = string.Empty;
+                    var isImportedScript = path.Contains("ThirdParty");
+                    var copyright = isImportedScript || string.IsNullOrEmpty(Copyright) ? string.Empty : "// " + Copyright;
+                    if (!isImportedScript || wrapNamespace) scriptText += copyright + Environment.NewLine + Environment.NewLine + "namespace " + NamespaceToWrap + Environment.NewLine + "{" + Environment.NewLine;
+                    scriptText += isImportedScript ? originalScriptText : TAB_CHARS + originalScriptText.Replace(Environment.NewLine, Environment.NewLine + TAB_CHARS);
+                    if (!isImportedScript || wrapNamespace) scriptText += Environment.NewLine + "}" + Environment.NewLine;
+                    File.WriteAllText(fullpath, scriptText, Encoding.UTF8);
+    
+                    modifiedScripts.Add(fullpath, originalScriptText);
+                }
+            }
+    
+            AssetDatabase.ExportPackage(AssetsPath, OutputPath + "/" + OutputFileName + ".unitypackage", ExportPackageOptions.Recurse);
+    
+            if (!string.IsNullOrEmpty(NamespaceToWrap))
+            {
+                foreach (var modifiedScript in modifiedScripts)
+                    File.WriteAllText(modifiedScript.Key, modifiedScript.Value, Encoding.UTF8);
             }
         }
     }
+    
 }
