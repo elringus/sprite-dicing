fb7692ce1e795f683e97668ccb079881e6cdd668
Artyom Sovetnikov <elringus@gmail.com>
2021-04-29 22:46:34 +0300

    remove exclude

--- Assets/Tests/Editor/Helpers.cs
+++ Assets/Tests/Editor/Helpers.cs
@@ -2,11 +2,9 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.TestTools;
 
 namespace SpriteDicing.Test
 {
-    [ExcludeFromCoverage]
     public static class Helpers
     {
         public static class Paths
