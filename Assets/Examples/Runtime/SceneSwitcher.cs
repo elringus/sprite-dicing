5441c4e705472b94ee7e1c5d12ce304b11a9d400
Artyom Sovetnikov <elringus@gmail.com>
2021-10-05 18:16:56 +0300

    add codecov (#14)

---
+++ Assets/Examples/Runtime/SceneSwitcher.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    private const int buttonHeight = 50;
+    private const int buttonWidth = 150;
+
+    private void OnGUI ()
+    {
+        for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
+            if (GUI.Button(GetRectForSceneAt(i), GetNameForSceneAt(i)))
+                SceneManager.LoadScene(i);
+    }
+
+    private static Rect GetRectForSceneAt (int index)
+    {
+        var yPos = Screen.height - (buttonHeight + buttonHeight * index);
+        return new Rect(0, yPos, buttonWidth, buttonHeight);
+    }
+
+    private static string GetNameForSceneAt (int index)
+    {
+        var scenePath = SceneUtility.GetScenePathByBuildIndex(index);
+        return Path.GetFileNameWithoutExtension(scenePath);
+    }
+}
