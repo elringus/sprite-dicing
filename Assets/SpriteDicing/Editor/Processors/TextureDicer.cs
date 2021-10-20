9a8675dc407e1cbc60ff4b7012384844f5c8270f
Artyom Sovetnikov <elringus@gmail.com>
2021-10-20 20:08:49 +0300

    refactor dicer (#17)

--- Assets/SpriteDicing/Editor/Processors/TextureDicer.cs
+++ Assets/SpriteDicing/Editor/Processors/TextureDicer.cs
@@ -12,6 +12,9 @@ namespace SpriteDicing
     {
         private readonly int unitSize;
         private readonly int padding;
+        private readonly List<DicedUnit> units = new List<DicedUnit>();
+
+        private Texture2D texture;
 
         public TextureDicer (int unitSize, int padding)
         {
@@ -24,32 +27,35 @@ namespace SpriteDicing
 
         public DicedTexture Dice (SourceTexture source)
         {
-            var texture = source.Texture;
-            var units = new List<DicedUnit>();
+            PrepareToDice(source);
             var unitCountX = Mathf.CeilToInt((float)texture.width / unitSize);
             var unitCountY = Mathf.CeilToInt((float)texture.height / unitSize);
-
             for (int unitX = 0; unitX < unitCountX; unitX++)
             for (int unitY = 0; unitY < unitCountY; unitY++)
-            {
-                var x = unitX * unitSize;
-                var y = unitY * unitSize;
-                var pixelsRect = new RectInt(x, y, unitSize, unitSize);
-                var pixels = GetPixels(texture, pixelsRect);
-                if (pixels.All(p => p.a == 0)) continue;
-                var paddedRect = PadRect(pixelsRect, padding);
-                var paddedPixels = GetPixels(texture, paddedRect);
-                var quadVerts = CropOverBorders(pixelsRect, x, y, texture);
-                var hash = GetHash(unitSize, pixels);
-                var dicedUnit = new DicedUnit(quadVerts, paddedPixels, hash);
-                units.Add(dicedUnit);
-            }
-
+                DiceAt(unitX * unitSize, unitY * unitSize);
             return new DicedTexture(source, units);
         }
 
+        private void PrepareToDice (SourceTexture source)
+        {
+            units.Clear();
+            texture = source.Texture;
+        }
+
+        private void DiceAt (int x, int y)
+        {
+            var pixelsRect = new RectInt(x, y, unitSize, unitSize);
+            var pixels = GetPixels(pixelsRect);
+            if (pixels.All(p => p.a == 0)) return;
+            var paddedRect = PadRect(pixelsRect);
+            var paddedPixels = GetPixels(paddedRect);
+            var quadVerts = CropOverBorders(pixelsRect, x, y);
+            var hash = GetHash(unitSize, pixels);
+            units.Add(new DicedUnit(quadVerts, paddedPixels, hash));
+        }
+
         /// <returns>Flattened 2D array, where pixels are laid out left to right, bottom to top.</returns>
-        private static Color[] GetPixels (Texture2D texture, RectInt rect)
+        private Color[] GetPixels (RectInt rect)
         {
             // TODO: GetPixels() from texture and reuse the array.
             var endX = rect.x + rect.width;
@@ -61,13 +67,13 @@ namespace SpriteDicing
             return colors;
         }
 
-        private static RectInt PadRect (RectInt rect, int padding)
+        private RectInt PadRect (RectInt rect)
         {
             var delta = Vector2Int.one * padding;
             return new RectInt(rect.position - delta, rect.size + delta * 2);
         }
 
-        private static RectInt CropOverBorders (RectInt rect, int x, int y, Texture texture)
+        private RectInt CropOverBorders (RectInt rect, int x, int y)
         {
             rect.width = Mathf.Min(rect.width, texture.width - x);
             rect.height = Mathf.Min(rect.height, texture.height - y);
