67b281305881e19a08a0458e02f66d4c1ab216fe
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2024-03-07 19:19:49 +0300

    docs

--- crates/lib/src/lib.rs
+++ crates/lib/src/lib.rs
@@ -1,5 +1,5 @@
 //! Provides API for generating atlas textures and diced sprite meshes from/to raw pixels.
-//! When `fs` features is enabled, additionally provides APIs to read and decode various
+//! When `fs` feature is enabled, additionally provides APIs to read and decode various
 //! texture formats from the file system as well as to write generated atlases into textures
 //! of various formats and diced sprite meshes into OBJ files.
 
