51ff2cd1bdf0495f2ccddfc64534b3c1fad19a2f
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2024-03-07 19:20:32 +0300

    docs

--- crates/lib/src/lib.rs
+++ crates/lib/src/lib.rs
@@ -1,7 +1,7 @@
 //! Provides API for generating atlas textures and diced sprite meshes from/to raw pixels.
 //! When `fs` feature is enabled, additionally provides APIs to read and decode various
 //! texture formats from the file system as well as to write generated atlases into textures
-//! of various formats and diced sprite meshes into OBJ files.
+//! of various formats and diced sprite meshes into Wavefront OBJ files.
 
 #[cfg(feature = "fs")]
 mod fs;
