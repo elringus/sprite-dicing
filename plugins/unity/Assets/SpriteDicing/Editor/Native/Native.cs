05d9da3065596fa7a4ec9fbfc91575e3c4493f47
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2024-03-29 19:28:14 +0300

    iteration

--- plugins/unity/Assets/SpriteDicing/Editor/Native/Native.cs
+++ plugins/unity/Assets/SpriteDicing/Editor/Native/Native.cs
@@ -48,18 +48,18 @@ namespace SpriteDicing
             public IReadOnlyList<byte[]> Atlases { get; }
             public IReadOnlyList<DicedSprite> Sprites { get; }
 
-            private readonly IntPtr[] pointers;
+            private readonly List<IntPtr> pts;
 
-            internal Artifacts (byte[][] atlases, DicedSprite[] sprites, IntPtr[] pointers)
+            internal Artifacts (byte[][] atlases, DicedSprite[] sprites, List<IntPtr> pts)
             {
                 Atlases = atlases;
                 Sprites = sprites;
-                this.pointers = pointers;
+                this.pts = pts;
             }
 
             public void Dispose ()
             {
-                foreach (var ptr in pointers)
+                foreach (var ptr in pts)
                     Marshal.FreeHGlobal(ptr);
             }
         }
@@ -102,7 +102,7 @@ namespace SpriteDicing
         }
 
         [StructLayout(LayoutKind.Sequential)]
-        private struct CSprite
+        private struct CSourceSprite
         {
             public IntPtr id;
             public CSlice bytes;
@@ -188,67 +188,35 @@ namespace SpriteDicing
 
         public static Artifacts Dice (IEnumerable<SourceSprite> sprites, Prefs prefs)
         {
-            var cBytes = new List<GCHandle>();
-            var cSprites = Pin(sprites.Select(s => {
-                var bytes = Pin(s.Bytes);
-                cBytes.Add(bytes);
-                return new CSprite {
-                    id = Marshal.StringToHGlobalAnsi(s.Id),
-                    bytes = new CSlice {
-                        ptr = bytes.AddrOfPinnedObject(),
-                        len = (ulong)((byte[])bytes.Target).Length
-                    },
-                    format = Marshal.StringToHGlobalAnsi(s.Format),
-                    has_pivot = s.Pivot.HasValue,
-                    pivot = new CPivot { x = s.Pivot.GetValueOrDefault().X, y = s.Pivot.GetValueOrDefault().Y }
-                };
-            }).ToArray());
-            var cSliceOfSprites = new CSlice {
-                ptr = cSprites.AddrOfPinnedObject(),
-                len = (ulong)((CSprite[])cSprites.Target).Length
-            };
-            var diced = dice(cSliceOfSprites, MarshalPrefs(prefs));
-
-            cBytes.ForEach(c => c.Free());
-            cSprites.Free();
-
-            var atlasSlices = MarshalSlice<CSlice>(diced.atlases);
-            var atlases = atlasSlices.Select(s => MarshalBytes(s.ptr, (int)s.len)).ToArray();
-            var cDicedSprites = MarshalSlice<CDicedSprite>(diced.sprites);
-            var dicedSprites = cDicedSprites.Select(MarshalDicedSprite).ToArray();
-            var pointers = atlasSlices.Select(s => s.ptr)
-                .Append(diced.atlases.ptr)
-                .Append(diced.sprites.ptr)
-                .Concat(cDicedSprites.SelectMany(c => new[] {
-                    c.vertices.ptr,
-                    c.uvs.ptr,
-                    c.indices.ptr
-                }))
-                .ToArray();
-            return new Artifacts(atlases, dicedSprites, pointers);
+            var pts = new List<IntPtr>();
+            var pins = new List<GCHandle>();
+            var diced = dice(MarshalSourceSprites(sprites, pins), MarshalPrefs(prefs));
+            pins.ForEach(c => c.Free());
+            return new Artifacts(
+                MarshalAtlases(diced.atlases, pts),
+                MarshalDicedSprites(diced.sprites, pts), pts);
         }
 
         [DllImport("sprite_dicing")]
         private static extern CArtifacts dice (CSlice sprites, CPrefs prefs);
 
-        private static CPrefs MarshalPrefs (Prefs prefs)
-        {
-            return new CPrefs {
-                unit_size = prefs.UnitSize,
-                padding = prefs.Padding,
-                uv_inset = prefs.UVInset,
-                trim_transparent = prefs.TrimTransparent,
-                atlas_size_limit = prefs.AtlasSizeLimit,
-                atlas_square = prefs.AtlasSquare,
-                atlas_pot = prefs.AtlasPOT,
-                pivot = new CPivot { x = prefs.Pivot.X, y = prefs.Pivot.Y },
-                ppu = prefs.PPU,
-                atlas_format = (byte)prefs.AtlasFormat
-            };
-        }
+        private static CPrefs MarshalPrefs (Prefs prefs) => new CPrefs {
+            unit_size = prefs.UnitSize,
+            padding = prefs.Padding,
+            uv_inset = prefs.UVInset,
+            trim_transparent = prefs.TrimTransparent,
+            atlas_size_limit = prefs.AtlasSizeLimit,
+            atlas_square = prefs.AtlasSquare,
+            atlas_pot = prefs.AtlasPOT,
+            pivot = MarshalPivot(prefs.Pivot),
+            ppu = prefs.PPU,
+            atlas_format = (byte)prefs.AtlasFormat
+        };
 
-        private static T[] MarshalSlice<T> (CSlice c)
+        private static T[] MarshalSlice<T> (CSlice c, List<IntPtr> pts)
         {
+            pts.Add(c.ptr);
+
             var size = Marshal.SizeOf(typeof(T));
             var structs = new T[c.len];
 
@@ -261,25 +229,59 @@ namespace SpriteDicing
             return structs;
         }
 
-        private static byte[] MarshalBytes (IntPtr ptr, int length)
+        private static CSlice MarshalSlice<T> (T[] array, List<GCHandle> pins)
         {
-            var array = new byte[length];
-            Marshal.Copy(ptr, array, 0, length);
+            var pin = GCHandle.Alloc(array, GCHandleType.Pinned);
+            pins.Add(pin);
+            return new CSlice {
+                ptr = pin.AddrOfPinnedObject(),
+                len = (ulong)array.Length
+            };
+        }
+
+        private static byte[] MarshalBytes (CSlice c, List<IntPtr> pts)
+        {
+            pts.Add(c.ptr);
+            var array = new byte[c.len];
+            Marshal.Copy(c.ptr, array, 0, (int)c.len);
             return array;
         }
 
-        private static DicedSprite MarshalDicedSprite (CDicedSprite c)
+        private static byte[][] MarshalAtlases (CSlice c, List<IntPtr> pts)
         {
-            return new DicedSprite {
-                // TODO: Marshal.PtrToStringUTF8
-                Id = Marshal.PtrToStringAnsi(c.id),
-                Atlas = (int)c.atlas,
-                Vertices = MarshalSlice<CVertex>(c.vertices).Select(MarshalVertex).ToArray(),
-                UVs = MarshalSlice<CUV>(c.uvs).Select(MarshalUV).ToArray(),
-                Indices = MarshalIndices(c.indices),
-                Rect = MarshalRect(c.rect),
-                Pivot = MarshalPivot(c.Pivot)
-            };
+            var atlasSlices = MarshalSlice<CSlice>(c, pts);
+            return atlasSlices.Select(s => MarshalBytes(s, pts)).ToArray();
+        }
+
+        private static CSlice MarshalSourceSprites (IEnumerable<SourceSprite> sources, List<GCHandle> pins)
+        {
+            var sprites = sources.Select(s => MarshalSourceSprite(s, pins)).ToArray();
+            return MarshalSlice(sprites, pins);
+        }
+
+        private static CSourceSprite MarshalSourceSprite (SourceSprite s, List<GCHandle> pins) => new CSourceSprite {
+            id = Marshal.StringToHGlobalAnsi(s.Id),
+            bytes = MarshalSlice(s.Bytes, pins),
+            format = Marshal.StringToHGlobalAnsi(s.Format),
+            has_pivot = s.Pivot.HasValue,
+            pivot = new CPivot { x = s.Pivot.GetValueOrDefault().X, y = s.Pivot.GetValueOrDefault().Y }
+        };
+
+        private static DicedSprite MarshalDicedSprite (CDicedSprite c, List<IntPtr> pts) => new DicedSprite {
+            // TODO: Marshal.PtrToStringUTF8
+            Id = Marshal.PtrToStringAnsi(c.id),
+            Atlas = (int)c.atlas,
+            Vertices = MarshalSlice<CVertex>(c.vertices, pts).Select(MarshalVertex).ToArray(),
+            UVs = MarshalSlice<CUV>(c.uvs, pts).Select(MarshalUV).ToArray(),
+            Indices = MarshalIndices(c.indices),
+            Rect = MarshalRect(c.rect),
+            Pivot = MarshalPivot(c.Pivot)
+        };
+
+        private static DicedSprite[] MarshalDicedSprites (CSlice c, List<IntPtr> pts)
+        {
+            var sprites = MarshalSlice<CDicedSprite>(c, pts);
+            return sprites.Select(s => MarshalDicedSprite(s, pts)).ToArray();
         }
 
         private static Vertex MarshalVertex (CVertex c)
@@ -301,24 +303,21 @@ namespace SpriteDicing
             return array;
         }
 
-        private static Rect MarshalRect (CRect c)
-        {
-            return new Rect {
-                X = c.x,
-                Y = c.y,
-                Width = c.width,
-                Height = c.height
-            };
-        }
+        private static Rect MarshalRect (CRect c) => new Rect {
+            X = c.x,
+            Y = c.y,
+            Width = c.width,
+            Height = c.height
+        };
 
         private static Pivot MarshalPivot (CPivot c)
         {
             return new Pivot { X = c.x, Y = c.y };
         }
 
-        private static GCHandle Pin (object obj)
+        private static CPivot MarshalPivot (Pivot p)
         {
-            return GCHandle.Alloc(obj, GCHandleType.Pinned);
+            return new CPivot { x = p.X, y = p.Y };
         }
     }
 }
