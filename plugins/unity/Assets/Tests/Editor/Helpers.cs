60026ae8098eb9f6cc62c69f4ed4985568222c27
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2024-03-31 16:35:30 +0300

    etc

--- plugins/unity/Assets/Tests/Editor/Helpers.cs
+++ plugins/unity/Assets/Tests/Editor/Helpers.cs
@@ -43,10 +43,10 @@ namespace SpriteDicing.Test
 
         public static class Colors
         {
-            public static readonly Color32 Red = new Color32(255, 0, 0, 255);
-            public static readonly Color32 Green = new Color32(0, 255, 0, 255);
-            public static readonly Color32 Blue = new Color32(0, 0, 255, 255);
-            public static readonly Color32 Black = new Color32(0, 0, 0, 255);
+            public static readonly Color32 Red = new(255, 0, 0, 255);
+            public static readonly Color32 Green = new(0, 255, 0, 255);
+            public static readonly Color32 Blue = new(0, 0, 255, 255);
+            public static readonly Color32 Black = new(0, 0, 0, 255);
         }
 
         public const string TextureFolderPath = "Assets/Tests/Textures";
