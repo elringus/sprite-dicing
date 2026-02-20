af3b2180d1b192f5c4afe8e3a1db9b6c2281f5d0
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2026-02-20 15:26:15 +0300

    init

---
+++ plugins/godot/addons/sprite_dicing/sprite_dicing.gd
@@ -0,0 +1,18 @@
+@tool
+extends EditorPlugin
+
+func _enable_plugin() -> void:
+    # Add autoloads here.
+    pass
+
+func _disable_plugin() -> void:
+    # Remove autoloads here.
+    pass
+
+func _enter_tree() -> void:
+    # Initialization of the plugin goes here.
+    pass
+
+func _exit_tree() -> void:
+    # Clean-up of the plugin goes here.
+    pass    
