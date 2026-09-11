f4f6ba5a0fb41378598b0c1da00fdb778db09fbd
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2026-09-11 13:42:29 +0300

    feat(cli): publish cli to crates.io

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,18 +8,18 @@ unity=../plugins/unity/Assets/SpriteDicing/Editor/Native
 godot=../plugins/godot/addons/sprite_dicing/editor/native
 
 rm -rf $dist
-cargo build -p abi -p cli --release --target x86_64-pc-windows-msvc
+cargo build -p abi -p dicing --release --target x86_64-pc-windows-msvc
 MSYS_NO_PATHCONV=1 docker run --rm -t -v "$PWD:/io" -w /io messense/cargo-zigbuild:0.23.3 sh -c "
-cargo zigbuild -p abi -p cli --release --target x86_64-unknown-linux-gnu --target aarch64-apple-darwin"
+cargo zigbuild -p abi -p dicing --release --target x86_64-unknown-linux-gnu --target aarch64-apple-darwin"
 
 mkdir -p $dist
 cp target/x86_64-pc-windows-msvc/release/abi.dll $dist/sprite_dicing.dll
 cp target/x86_64-unknown-linux-gnu/release/libabi.so $dist/sprite_dicing.so
 cp target/aarch64-apple-darwin/release/libabi.dylib $dist/sprite_dicing.dylib
 cp abi/sprite_dicing.h $dist/sprite_dicing.h
-cp target/x86_64-pc-windows-msvc/release/cli.exe $dist/dice-windows-x64.exe
-cp target/x86_64-unknown-linux-gnu/release/cli $dist/dice-linux-x64
-cp target/aarch64-apple-darwin/release/cli $dist/dice-mac-arm
+cp target/x86_64-pc-windows-msvc/release/dicing.exe $dist/dice-windows-x64.exe
+cp target/x86_64-unknown-linux-gnu/release/dicing $dist/dice-linux-x64
+cp target/aarch64-apple-darwin/release/dicing $dist/dice-mac-arm
 
 cp $dist/sprite_dicing.{dll,so,dylib} $unity/
 cp $dist/sprite_dicing.{dll,so,dylib} $godot/bin/
